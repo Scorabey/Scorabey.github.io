7968ba9f22f31339103c8451365bbccf5ac0d453
Scorabey <150181735+Scorabey@users.noreply.github.com>
2026-09-01 17:06:29 +0300

    Clean up console logs in BubbleSort function
    Removed console log statements for cleaner output.

--- JS-Alg/BubbleSort.js
+++ JS-Alg/BubbleSort.js
@@ -1,17 +1,12 @@
 const BubbleSort = (arr) => {
     for(let i = 0; i < array.length; i++) {
-        console.log(`Vector: start iteration for: ${array}`);
-        console.log(`-------------------------------------`)
         for(let j = 0; j < (array.length - i - 1); j++) {
             if(array[j] > array[j + 1]) {
                 let swap = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = swap;
-
-                console.log(`Matrix: array: ${array}, Swaped item: ${array[j + 1]}`);
             }
         }
-        console.log(`-------------------------------------`)
     }
 
     return array;
